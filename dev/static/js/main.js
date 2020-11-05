a41677e95be8edf6b614c9aec879ea3cdecda4a7
Natalya Garaschenko <n.garashchenko@gov39.ru>
2020-11-05 21:16:29 +0200

    делаю мобилку

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
         slidesToScroll: 1,
         arrows: false,
         dots:true,
-        dotsClass: "top-slider__nav"
+        dotsClass: "top-slider__nav",
+        fade: true,
+        cssEase: 'linear'
     });
 
     lightHeader();
@@ -19,6 +21,7 @@ $(document).ready(function(){
         squarePic(".reviews__image-wrapper");
     });
 
+    //карточка товара
     $('.slider-single').slick({
         infinite: true,
         slidesToShow: 1,
@@ -26,7 +29,19 @@ $(document).ready(function(){
         arrows: false,
         dots:false,
         fade: true,
-        /*asNavFor: '.slider-nav'*/
+        dotsClass: "slider-dots",
+        responsive: [
+          {
+            breakpoint: 720,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+
+            }
+          },
+        ]
     });
 
     $('.slider-nav').slick({
@@ -40,7 +55,7 @@ $(document).ready(function(){
         dots: false
       });
 
-      $(".product__tabs").on("click", ".product__tab:not(.active)", function() {
+    $(".product__tabs").on("click", ".product__tab:not(.active)", function() {
         $(this)
           .addClass("active")
           .siblings()
@@ -65,16 +80,119 @@ $(document).ready(function(){
         $(".header-mobile").toggleClass("active");
     });
 
+    //Мобильное меню
+    $(".menu-mob__btn").on("click", function(){
+        let item = $(".menu-mob__btn").closest(".menu-mob__item--has-submenu");
+        let submenu = item.find(".menu-mob__submenu");
+        let submenuHeight = submenu[0].scrollHeight;
+        if(item.hasClass("active-item")){
+            item.removeClass("active-item");
+            submenu.css("height", "0px");
+        } else {
+            item.addClass("active-item");
+            submenu.css("height", submenuHeight + "px");
+        }
+        
+    });
+
+
+
+    /*ОФОРМЛЕНИЕ ЗАКАЗА*/
+
     //яндекс карты в оформлении заказа
     ymaps.ready(init);
 
+    /*кнопка сворачивает инфо о заказе в мобилке*/
+    $("#order-info-button").on("click", function(){
+        let orderInfo = $("#order-info");
+        let orderInfoBlock = $("#order-info-block");
+        let orderInfoBlockHeight = $("#order-info-block")[0].scrollHeight;
+        if(orderInfo.hasClass("active")) {
+            orderInfo.removeClass("active");
+            orderInfoBlock.css("height", "0px");
+        } else {
+            orderInfo.addClass("active");
+            orderInfoBlock.css("height", orderInfoBlockHeight + 30 + "px");
+        }
+    });
+
+    /*обеспечивает работу табов в мобилке*/
+    $(".order__tabs-mobile-item").on("click", function(e){
+        let target = e.target;
+        let tab = target.closest(".order__tabs-mobile-item");
+        let currentTabId = tab.dataset.tab;
+        $(".order__tabs-mobile-item").removeClass("active");
+        $(".order__tabs-mobile-info").each(function(){
+            if($(this).attr("data-tab") == currentTabId) {
+                $(this).addClass("active");
+            } else {
+                $(this).removeClass("active");
+            }
+        });
+        tab.classList.add("active");
+    });
+
+    /*Работа радио кнопок оплаты */
+    $("input[name='payment']").each(function(){
+        let select = $(this).closest(".order__select");
+        console.log(select)
+        if ($(this).checked) {
+            $(".order__select").removeClass("active");
+            select.classList.add("active");
+        }
+    });
+    $("input[name='payment']").on("change", function(e){
+        let target= e.target;
+        let select = target.closest(".order__select");
+        if (target.checked) {
+            $(".order__select").removeClass("active");
+            select.classList.add("active");
+        }
+    });
+    /*Работа радио кнопок доставки*/
+    $("input[name='delivery']").each(function(){
+        let select = $(this).closest(".order__select");
+        console.log(select)
+        if ($(this).checked) {
+            $(".order__select").removeClass("active");
+            select.classList.add("active");
+        }
+    });
+    $("input[name='delivery']").on("change", function(e){
+        let target= e.target;
+        let select = target.closest(".order__select");
+        if (target.checked) {
+            $(".order__select").removeClass("active");
+            select.classList.add("active");
+        }
+    });
+
+    $(".order__next").on("click", function(e){
+        e.preventDefault();
+        let currentTabId = $(this).attr("data-tab");
+
+        $(".order__tabs-mobile-info").each(function(){
+            if($(this).attr("data-tab") == currentTabId) {
+                $(this).addClass("active");
+            } else {
+                $(this).removeClass("active");
+            }
+        });
+        $(".order__tabs-mobile-item").each(function(){
+            if($(this).attr("data-tab") == currentTabId) {
+                $(this).addClass("active");
+            } else {
+                $(this).removeClass("active");
+            }
+        });
+    });
 });
 
 function lightHeader(){
     if($(window).scrollTop() > 300) {
-        $(".header").addClass("header--light")
+        $(".header").addClass("header--light");
     } else {
-        $(".header").removeClass("header--light")
+        $(".header").removeClass("header--light");
     }
 }
 
@@ -83,20 +201,13 @@ function squarePic(currentClass){
     $(currentClass).each(function(){
         let height = $(this).css("width");
         $(this).css("height", height);
-        //$(this).css("height", height);
     });
 }
 
 function init(){
     // Создание карты.
     var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
         center: [55.76, 37.64],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
         zoom: 7
     });
 }
